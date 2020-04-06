c4f1cddb42009d74c957bf6164db0e9209d6848e
Shrikant Malviya <s.kant.malviya@gmail.com>
2020-04-06 07:57:42 +0530

    included the flairNLP.

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Repo for generating various Word-Embeddings for a Hindi Corpus
 # Tutorial
 1. Training ELMO from Scratch on Custom Data-set for Generating Embeddings (Tensorflow) : https://appliedmachinelearning.blog/2019/11/30/training-elmo-from-scratch-on-custom-data-set-for-generating-embeddings-tensorflow/
 2. Deep Dive The ELMo Implementation : https://huntzhan.github.io/posts/nlp/elmo/
+3.How to build custom NER model with Context based Word Embeddings in Vernacular languages: https://medium.com/saarthi-ai/how-to-make-your-own-ner-model-with-contexual-word-embeddings-5086276e04a0
 
 **Task**
 * Follow the above tutorial.
 * The program 'training_swb.py' generates training, testing data from swb.zip wich contains transcript of switchboard corpus in order to evaluate a WE model.
-* Write program to build W2V (CBOW, SG), GLOVE, FASTTEST, BLIM-TF, FLAIR and compare them on swb.zip corpus before training our own corpus. 
+* Write program to build W2V (CBOW, SG), GLOVE, FASTTEST, BLIM-TF, FLAIR and compare them on swb.zip corpus before training our own corpus.
---
+++ flair/flair_hindi.py
@@ -0,0 +1,38 @@
+from flair.embeddings import WordEmbeddings, CharacterEmbeddings, FlairEmbeddings, StackedEmbeddings
+from flair.data import Sentence
+
+# Hindi embeddings trained with Wikipedia
+# https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/embeddings-v0.4/hi-wiki-fasttext-300d-1M.vectors.npy'
+# https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/embeddings-v0.4/hi-wiki-fasttext-300d-1M
+hindi_wikipedia_embeddings = WordEmbeddings('hi')
+
+# Hindi embeddings trained with Web crawls
+# https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/embeddings-v0.4/hi-crawl-fasttext-300d-1M.vectors.npy
+# https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/embeddings-v0.4/hi-crawl-fasttext-300d-1M
+hindi_webcrawl_embeddings = WordEmbeddings('hi-crawl')
+
+# init standard GloVe embedding
+# https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/embeddings-stefan-it/lm-hi-opus-large-forward-v0.1.pt
+hindi_flair_forward = FlairEmbeddings('hi-forward')
+# https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/embeddings-stefan-it/lm-hi-opus-large-backward-v0.1.pt
+hindi_flair_backword = FlairEmbeddings('hi-backward')
+
+
+# create a StackedEmbedding object that combines glove and forward/backward flair embeddings
+'''
+stacked_embeddings = StackedEmbeddings([
+                                        glove_embedding,
+                                        flair_embedding_forward,
+                                        flair_embedding_backward,
+                                       ])
+'''
+sentence = Sentence('घास हरी है । ')
+
+
+# just embed a sentence using the StackedEmbedding as you would with any single embedding.
+hindi_wikipedia_embeddings.embed(sentence)
+
+# now check out the embedded tokens.
+for token in sentence:
+    print(token)
+    print(token.embedding, token.embedding.shape)
